d57e817fddbb42dccb9e5b2bf279eeebb87ae4d4
John Dias <joaodias@google.com>
2016-08-12 08:36:08 -0700

    binder: Set binder_(alloc_)debug_mask=0 to suppress logging.
     * Excessive logging -- not present on angler -- is
       affecting performance, contributing to missed audio
       deadlines and likely other latency-dependent tasks.
    
    Bug: 30375418
    [0ctobot: Extend patch to cover binder_alloc_debug_mask]
    Co-authored-by: Adam W. Willis <return.of.octobot@gmail.com>
    
    Change-Id: I36c4ce8a0294e5e7847a3fd884691cb178cd95c2
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -37,8 +37,7 @@ enum {
 	BINDER_DEBUG_BUFFER_ALLOC_ASYNC     = 1U << 3,
 };
 
-#ifdef CONFIG_ANDROID_BINDER_LOGS
-static uint32_t binder_alloc_debug_mask = BINDER_DEBUG_USER_ERROR;
+static uint32_t binder_alloc_debug_mask = 0;
 
 module_param_named(debug_mask, binder_alloc_debug_mask,
 		   uint, 0644);
