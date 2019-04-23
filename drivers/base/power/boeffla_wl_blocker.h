d128f922294d3f241fa8c42c0627d99148febb39
Russell King <rmk+kernel@armlinux.org.uk>
2019-04-23 17:09:38 +0100

    ARM: fix function graph tracer and unwinder dependencies
    commit 503621628b32782a07b2318e4112bd4372aa3401 upstream.
    
    Naresh Kamboju recently reported that the function-graph tracer crashes
    on ARM. The function-graph tracer assumes that the kernel is built with
    frame pointers.
    
    We explicitly disabled the function-graph tracer when building Thumb2,
    since the Thumb2 ABI doesn't have frame pointers.
    
    We recently changed the way the unwinder method was selected, which
    seems to have made it more likely that we can end up with the function-
    graph tracer enabled but without the kernel built with frame pointers.
    
    Fix up the function graph tracer dependencies so the option is not
    available when we have no possibility of having frame pointers, and
    adjust the dependencies on the unwinder option to hide the non-frame
    pointer unwinder options if the function-graph tracer is enabled.
    
    Reviewed-by: Masami Hiramatsu <mhiramat@kernel.org>
    Tested-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Reported-by: Danilo Cezar Zanella <danilo.zanella@iag.usp.br>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -17,9 +17,9 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.2.0"
 
-#define LIST_WL_DEFAULT	"wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;wlan_ipa;wlan_pno_wl;wcnss_filter_lock;[timerfd];IPA_CLIENT_APPS_LAN_CONS;IPA_CLIENT_APPS_WAN_CONS;rmnet_ipa%d;IPCRTR_lpass_rx;hal_bluetooth_lock;DIAG_WS;qcom_sap_wakelock;sensor_ind;wlan_rx_wake;wlan_ctrl_wake;wlan_wake;qbt_wake_source;998000.qcom,qup_uart"
+#define LIST_WL_DEFAULT	"wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;wlan_ipa;wlan_pno_wl;wcnss_filter_lock;[timerfd];IPA_CLIENT_APPS_LAN_CONS;IPA_CLIENT_APPS_WAN_CONS;rmnet_ipa%d;IPCRTR_lpass_rx;hal_bluetooth_lock;DIAG_WS;qcom_sap_wakelock;sensor_ind;wlan_rx_wake"
 #define LENGTH_LIST_WL_DEFAULT	 sizeof(LIST_WL_DEFAULT)
-#define LENGTH_LIST_WL		 2055
+#define LENGTH_LIST_WL		 255
 #define LENGTH_LIST_WL_SEARCH	 LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
 
 extern void wakeup_source_deactivate(struct wakeup_source *ws);
