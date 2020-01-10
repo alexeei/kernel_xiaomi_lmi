2fa4115a5b0be9b424ddab7e5db93590ae62fe84
Richard Henderson <richard.henderson@linaro.org>
2020-01-10 14:54:17 +0000

    linux/random.h: Use false with bool
    commit 66f5ae899ada79c0e9a3d8d954f93a72344cd350 upstream.
    
    Keep the generic fallback versions in sync with the other architecture
    specific implementations and use the proper name for false.
    
    Suggested-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20200110145422.49141-6-broonie@kernel.org
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/random.h
+++ include/linux/random.h
@@ -132,6 +132,11 @@ static inline bool __must_check arch_get_random_int(unsigned int *v)
 static inline bool arch_get_random_seed_long(unsigned long *v)
 {
 	return false;
+
+}
+static inline bool arch_get_random_seed_int(unsigned int *v)
+{
+	return false;
 }
 #endif
 
