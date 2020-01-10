afc7d2b78b9cffa0d66310cb52f988b6f9a9724d
Richard Henderson <richard.henderson@linaro.org>
2020-01-10 14:54:16 +0000

    linux/random.h: Remove arch_has_random, arch_has_random_seed
    commit 647f50d5d9d933b644b29c54f13ac52af1b1774d upstream.
    
    The arm64 version of archrandom.h will need to be able to test for
    support and read the random number without preemption, so a separate
    query predicate is not practical.
    
    Since this part of the generic interface is unused, remove it.
    
    Signed-off-by: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20200110145422.49141-5-broonie@kernel.org
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/random.h
+++ include/linux/random.h
@@ -128,11 +128,8 @@ static inline bool __must_check arch_get_random_int(unsigned int *v)
 {
 	return false;
 }
-static inline bool __must_check arch_get_random_seed_long(unsigned long *v)
-{
-	return false;
-}
-static inline bool __must_check arch_get_random_seed_int(unsigned int *v)
+
+static inline bool arch_get_random_seed_long(unsigned long *v)
 {
 	return false;
 }
@@ -148,14 +145,7 @@ static inline bool __init arch_get_random_seed_long_early(unsigned long *v)
 	WARN_ON(system_state != SYSTEM_BOOTING);
 	return arch_get_random_seed_long(v);
 }
-#endif
 
-#ifndef arch_get_random_long_early
-static inline bool __init arch_get_random_long_early(unsigned long *v)
-{
-	WARN_ON(system_state != SYSTEM_BOOTING);
-	return arch_get_random_long(v);
-}
 #endif
 
 #endif /* _LINUX_RANDOM_H */
