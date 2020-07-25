f6eff27ea3ac13f9e7526b30cc79003ffe65f644
alk3pInjection <webmaster@raspii.tech>
2020-07-25 16:06:02 +0800

    fs: {ext4, f2fs}: Remove CAF tracings
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    [ Tashar02: Adapt to latest f2fs upstream changes ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -36,7 +36,6 @@
 #include "gc.h"
 #include "iostat.h"
 #include <trace/events/f2fs.h>
-#include <trace/events/android_fs.h>
 #include <uapi/linux/f2fs.h>
 
 static vm_fault_t f2fs_filemap_fault(struct vm_fault *vmf)
@@ -281,16 +280,6 @@ static int f2fs_do_sync_file(struct file *file, loff_t start, loff_t end,
 
 	trace_f2fs_sync_file_enter(inode);
 
-	if (trace_android_fs_fsync_start_enabled()) {
-		char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		path = android_fstrace_get_pathname(pathbuf,
-				MAX_TRACE_PATHBUF_LEN, inode);
-		trace_android_fs_fsync_start(inode,
-				current->pid, path, current->comm);
-	}
-	start_time = ktime_get();
-
 	if (S_ISDIR(inode->i_mode))
 		goto go_write;
 
@@ -412,32 +401,8 @@ flush_out:
 	}
 	f2fs_update_time(sbi, REQ_TIME);
 out:
-#if defined(CONFIG_UFSTW) && defined(UFS3V0)
-	if (turbo_set)
-		bdev_clear_turbo_write(sbi->sb->s_bdev);
-#endif
-
-	delta = ktime_sub(ktime_get(), start_time);
-	duration = (unsigned long long) ktime_to_ns(delta) / (1000 * 1000);
-
-	/* print slow fsync spend more than 1s */
-	if (duration > 1000) {
-		char *file_path, file_pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		file_path = android_fstrace_get_pathname(file_pathbuf,
-				MAX_TRACE_PATHBUF_LEN, inode);
-		pr_info("[f2fs] slow fsync: %llu ms, cp_reason: %s, "
-			"datasync = %d, ret = %d, comm: %s: (uid %u, gid %u), "
-			"entry: %s", duration, f2fs_cp_reasons[cp_reason],
-			datasync, ret, current->comm,
-			from_kuid_munged(&init_user_ns, current_fsuid()),
-			from_kgid_munged(&init_user_ns, current_fsgid()),
-			file_path);
-	}
-
 	trace_f2fs_sync_file_exit(inode, cp_reason, datasync, ret);
-	stat_inc_sync_file_count(sbi);
-	trace_android_fs_fsync_end(inode, start, end - start);
+
 	return ret;
 }
 
