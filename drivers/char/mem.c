126962971df332e32e0c4afd3660610e4aee77bd
Jebaitedneko <Jebaitedneko@gmail.com>
2020-12-16 17:28:36 +0530

    char: mem: register /dev/hw_random through devmem framework when HW_RANDOM is unset
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -895,6 +895,16 @@ static const struct memdev {
 	 [7] = { "full", 0666, &full_fops, 0 },
 	 [8] = { "random", 0666, &random_fops, 0 },
 	 [9] = { "urandom", 0666, &urandom_fops, 0 },
+
+	#endif
+	#ifndef CONFIG_HW_RANDOM
+	#ifndef CONFIG_SRANDOM
+	 [10] = { "hw_random", 0666, &urandom_fops, 0 },
+	#else
+	 [10] = { "hw_random", 0666, &sfops, 0 },
+	#endif
+	#endif
+
 #ifdef CONFIG_PRINTK
 	[11] = { "kmsg", 0644, &kmsg_fops, 0 },
 #endif
