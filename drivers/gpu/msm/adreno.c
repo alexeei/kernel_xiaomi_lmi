08462d8c8044ce9a9564b09ed9efe9ab99c5fba8
Hareesh Gundu <hareeshg@codeaurora.org>
2021-04-07 19:56:55 +0530

    BACKPORT: msm: kgsl: Add support to get gpu model from device tree
    We would have different gpu model name for different hardware
    specification. Add support to get gpu model properties from
    device tree against gpu hardware configuration.
    
    Change-Id: I85c4e1b1354ed63713535d5f01f6633138516de7
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1375,6 +1375,7 @@ static const char *adreno_get_gpu_model(struct kgsl_device *device)
 	return gpu_model;
 }
 
+
 static u32 adreno_get_vk_device_id(struct kgsl_device *device)
 {
 	struct device_node *node;
@@ -1394,6 +1395,7 @@ static u32 adreno_get_vk_device_id(struct kgsl_device *device)
 
 	return device_id;
 }
+
 
 static int adreno_probe_efuse(struct platform_device *pdev,
 					struct adreno_device *adreno_dev)
