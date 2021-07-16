c7902a06e7a4174e57c3c0eee1cd46355808d0e8
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-07-16 17:51:45 +0200

    input: aw8697_haptic: Disable Debugging
    Change-Id: I463a78cecfb19c50f76ffa747a072aac3bb4ff77
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/misc/aw8697_haptic/aw8697.c
+++ drivers/input/misc/aw8697_haptic/aw8697.c
@@ -16,7 +16,6 @@
  * Free Software Foundation;  either version 2 of the  License, or (at your
  * option) any later version.
  */
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/i2c.h>
