c771daafa62ca3c2e2e91e4b397baa19a2018b34
Quentin Perret <qperret@google.com>
2021-10-18 14:31:38 +0530

    cpufreq: Register governors at core_initcall
    Currently, most CPUFreq governors are registered at the core_initcall
    time when the given governor is the default one, and the module_init
    time otherwise.
    
    In preparation for letting users specify the default governor on the
    kernel command line, change all of them to be registered at the
    core_initcall unconditionally, as it is already the case for the
    schedutil and performance governors. This will allow us to assume
    that builtin governors have been registered before the built-in
    CPUFreq drivers probe.
    
    And since all governors have similar init/exit patterns now, introduce
    two new macros, cpufreq_governor_{init,exit}(), to factorize the code.
    
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    [ rjw: Changelog ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/cpufreq/cpufreq_performance.c
+++ drivers/cpufreq/cpufreq_performance.c
@@ -28,16 +28,13 @@ static struct cpufreq_governor cpufreq_gov_performance = {
 	.limits		= cpufreq_gov_performance_limits,
 };
 
-static int __init cpufreq_gov_performance_init(void)
-{
-	return cpufreq_register_governor(&cpufreq_gov_performance);
-}
 
-static void __exit cpufreq_gov_performance_exit(void)
+#ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
+struct cpufreq_governor *cpufreq_default_governor(void)
 {
-	cpufreq_unregister_governor(&cpufreq_gov_performance);
+	return &cpufreq_gov_performance;
 }
-
+#endif
 
 #ifndef CONFIG_CPU_FREQ_GOV_PERFORMANCE_MODULE
 struct cpufreq_governor *cpufreq_fallback_governor(void)
@@ -50,5 +47,5 @@ MODULE_AUTHOR("Dominik Brodowski <linux@brodo.de>");
 MODULE_DESCRIPTION("CPUfreq policy governor 'performance'");
 MODULE_LICENSE("GPL");
 
-fs_initcall(cpufreq_gov_performance_init);
-module_exit(cpufreq_gov_performance_exit);
+cpufreq_governor_init(cpufreq_gov_performance);
+cpufreq_governor_exit(cpufreq_gov_performance);
