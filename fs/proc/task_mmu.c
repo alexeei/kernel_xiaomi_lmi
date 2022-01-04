Jebaitedneko/android_kernel_xiaomi_vayu

mm: add sysfs switch driver for sultan pid & shrink
depends on "mm: Micro-optimize PID map reads for arm64 while retaining output format"
depends on "mm: Speed up PID map reads for Android's use case"

Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

fs/proc/task_mmu.c
@@ -24,6 +24,7 @@
 #include <asm/elf.h>
 #include <asm/tlb.h>
 #include <asm/tlbflush.h>
+#include <misc/lyb_taskmmu.h>
 #include "internal.h"
 
 void task_mem(struct seq_file *m, struct mm_struct *mm)
@@ -397,6 +398,124 @@ static int is_stack(struct vm_area_struct *vma)
 	2;								\
 })
 
+#define print_vma_hex10_shrink(out, val, clz_fn) \
+({									\
+	const typeof(val) __val = val;					\
+	char *const __out = out;					\
+	size_t __len;							\
+									\
+	if (__val) {							\
+		__len = (sizeof(__val) * 8 - clz_fn(__val) + 3) / 4;	\
+		switch (__len) {					\
+		case 10:						\
+			__out[9] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[8] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[7] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[6] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[5] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[4] = hex_asc[(__val >> 20) & 0xf];	\
+			__out[3] = hex_asc[(__val >> 24) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 28) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 32) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 36) & 0xf];	\
+			break;						\
+		case 9:							\
+			__out[8] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[7] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[6] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[5] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[4] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[3] = hex_asc[(__val >> 20) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 24) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 28) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 32) & 0xf];	\
+			break;						\
+		case 8:							\
+			__out[7] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[6] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[5] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[4] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[3] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 20) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 24) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 28) & 0xf];	\
+			break;						\
+		case 7:							\
+			__out[6] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[5] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[4] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[3] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 20) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 24) & 0xf];	\
+			break;						\
+		case 6:							\
+			__out[5] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[4] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[3] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 20) & 0xf];	\
+			break;						\
+		case 5:							\
+			__out[4] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[3] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[2] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 16) & 0xf];	\
+			break;						\
+		case 4:							\
+			__out[3] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[2] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[1] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 12) & 0xf];	\
+			break;						\
+		case 3:							\
+			__out[2] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[1] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[0] = hex_asc[(__val >>  8) & 0xf];	\
+			break;						\
+		case 2:							\
+			__out[1] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[0] = hex_asc[(__val >>  4) & 0xf];	\
+			break;						\
+		case 1:							\
+			__out[0] = hex_asc[(__val >>  0) & 0xf];	\
+			break;						\
+		}							\
+	} else {							\
+		__len = 1;						\
+		__out[0] = '0';						\
+	}								\
+									\
+	__len;								\
+})
+
+#define print_vma_hex2_shrink(out, val, clz_fn) \
+({									\
+	const typeof(val) __val = val;					\
+	char *const __out = out;					\
+	size_t __len;							\
+									\
+	if (__val) {							\
+		__len = (sizeof(__val) * 8 - clz_fn(__val) + 3) / 4;	\
+		switch (__len) {					\
+		case 2:							\
+			__out[1] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[0] = hex_asc[(__val >>  4) & 0xf];	\
+			break;						\
+		case 1:							\
+			__out[0] = hex_asc[(__val >>  0) & 0xf];	\
+			break;						\
+		}							\
+	} else {							\
+		__len = 1;						\
+		__out[0] = '0';						\
+	}								\
+									\
+	__len;								\
+})
+
 static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 				  unsigned long end, vm_flags_t flags,
 				  unsigned long long pgoff, dev_t dev,
@@ -414,30 +533,67 @@ static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 	/* Supports printing up to 40 bits per virtual address */
 	BUILD_BUG_ON(CONFIG_ARM64_VA_BITS > 40);
 
-	len = print_vma_hex10(out, start, __builtin_clzl);
+	if (lyb_sultan_pid_shrink)
+	{
+		/* 
+		 * shrinks the PID map output to be as small as
+		 * possible by omitting non-significant leading zeros from
+		 * hex output.
+		 */
+		len = print_vma_hex10_shrink(out, start, __builtin_clzl);
 
-	out[len++] = '-';
+		out[len++] = '-';
 
-	len += print_vma_hex10(out + len, end, __builtin_clzl);
+		len += print_vma_hex10_shrink(out + len, end, __builtin_clzl);
 
-	out[len++] = ' ';
-	out[len++] = "-r"[!!(flags & VM_READ)];
-	out[len++] = "-w"[!!(flags & VM_WRITE)];
-	out[len++] = "-x"[!!(flags & VM_EXEC)];
-	out[len++] = "ps"[!!(flags & VM_MAYSHARE)];
-	out[len++] = ' ';
+		out[len++] = ' ';
+		out[len++] = "-r"[!!(flags & VM_READ)];
+		out[len++] = "-w"[!!(flags & VM_WRITE)];
+		out[len++] = "-x"[!!(flags & VM_EXEC)];
+		out[len++] = "ps"[!!(flags & VM_MAYSHARE)];
+		out[len++] = ' ';
 
-	len += print_vma_hex10(out + len, pgoff, __builtin_clzll);
+		len += print_vma_hex10_shrink(out + len, pgoff, __builtin_clzll);
 
-	out[len++] = ' ';
+		out[len++] = ' ';
 
-	len += print_vma_hex2(out + len, MAJOR(dev));
+		len += print_vma_hex2_shrink(out + len, MAJOR(dev), __builtin_clz);
 
-	out[len++] = ':';
+		out[len++] = ':';
 
-	len += print_vma_hex2(out + len, MINOR(dev));
+		len += print_vma_hex2_shrink(out + len, MINOR(dev), __builtin_clz);
 
-	out[len++] = ' ';
+		out[len++] = ' ';
+	} else {
+		/* 
+		 * retains insignificant leading zeros from printed hex values	
+		 * to maintain the current output format.
+		 */
+		len = print_vma_hex10(out, start, __builtin_clzl);
+
+		out[len++] = '-';
+
+		len += print_vma_hex10(out + len, end, __builtin_clzl);
+
+		out[len++] = ' ';
+		out[len++] = "-r"[!!(flags & VM_READ)];
+		out[len++] = "-w"[!!(flags & VM_WRITE)];
+		out[len++] = "-x"[!!(flags & VM_EXEC)];
+		out[len++] = "ps"[!!(flags & VM_MAYSHARE)];
+		out[len++] = ' ';
+
+		len += print_vma_hex10(out + len, pgoff, __builtin_clzll);
+
+		out[len++] = ' ';
+
+		len += print_vma_hex2(out + len, MAJOR(dev));
+
+		out[len++] = ':';
+
+		len += print_vma_hex2(out + len, MINOR(dev));
+		
+		out[len++] = ' ';
+	}
 
 	len += num_to_str(&out[len], 20, ino);
 
@@ -603,6 +759,13 @@ static void *m_next_pid(struct seq_file *m, void *v, loff_t *pos)
 }
 
 static const struct seq_operations proc_pid_maps_op = {
+	.start	= m_start,
+	.next	= m_next,
+	.stop	= m_stop,
+	.show	= show_pid_map
+};
+
+static const struct seq_operations proc_pid_maps_op_sultanpid = {
 	.start	= m_start_pid,
 	.next	= m_next_pid,
 	.stop	= m_stop,
@@ -618,7 +781,9 @@ static const struct seq_operations proc_tid_maps_op = {
 
 static int pid_maps_open(struct inode *inode, struct file *file)
 {
-	return do_maps_open(inode, file, &proc_pid_maps_op);
+	if (lyb_sultan_pid)
+		return do_maps_open(inode, file, &proc_pid_maps_op_sultanpid);
+	else return do_maps_open(inode, file, &proc_pid_maps_op);
 }
 
 static int tid_maps_open(struct inode *inode, struct file *file)

alexeei/kernel_xiaomi_lmi

Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

fs/proc/task_mmu.c
@@ -25,6 +25,7 @@
 #include <asm/elf.h>
 #include <asm/tlb.h>
 #include <asm/tlbflush.h>
+#include <misc/lyb_taskmmu.h>
 #include "internal.h"
 
 #define SEQ_PUT_DEC(str, val) \
@@ -397,6 +398,124 @@ static int is_stack(struct vm_area_struct *vma)
 	2;								\
 })
 
+#define print_vma_hex10_shrink(out, val, clz_fn) \
+({									\
+	const typeof(val) __val = val;					\
+	char *const __out = out;					\
+	size_t __len;							\
+									\
+	if (__val) {							\
+		__len = (sizeof(__val) * 8 - clz_fn(__val) + 3) / 4;	\
+		switch (__len) {					\
+		case 10:						\
+			__out[9] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[8] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[7] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[6] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[5] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[4] = hex_asc[(__val >> 20) & 0xf];	\
+			__out[3] = hex_asc[(__val >> 24) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 28) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 32) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 36) & 0xf];	\
+			break;						\
+		case 9:							\
+			__out[8] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[7] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[6] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[5] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[4] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[3] = hex_asc[(__val >> 20) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 24) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 28) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 32) & 0xf];	\
+			break;						\
+		case 8:							\
+			__out[7] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[6] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[5] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[4] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[3] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 20) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 24) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 28) & 0xf];	\
+			break;						\
+		case 7:							\
+			__out[6] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[5] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[4] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[3] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 20) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 24) & 0xf];	\
+			break;						\
+		case 6:							\
+			__out[5] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[4] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[3] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[2] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 16) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 20) & 0xf];	\
+			break;						\
+		case 5:							\
+			__out[4] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[3] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[2] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[1] = hex_asc[(__val >> 12) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 16) & 0xf];	\
+			break;						\
+		case 4:							\
+			__out[3] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[2] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[1] = hex_asc[(__val >>  8) & 0xf];	\
+			__out[0] = hex_asc[(__val >> 12) & 0xf];	\
+			break;						\
+		case 3:							\
+			__out[2] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[1] = hex_asc[(__val >>  4) & 0xf];	\
+			__out[0] = hex_asc[(__val >>  8) & 0xf];	\
+			break;						\
+		case 2:							\
+			__out[1] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[0] = hex_asc[(__val >>  4) & 0xf];	\
+			break;						\
+		case 1:							\
+			__out[0] = hex_asc[(__val >>  0) & 0xf];	\
+			break;						\
+		}							\
+	} else {							\
+		__len = 1;						\
+		__out[0] = '0';						\
+	}								\
+									\
+	__len;								\
+})
+
+#define print_vma_hex2_shrink(out, val, clz_fn) \
+({									\
+	const typeof(val) __val = val;					\
+	char *const __out = out;					\
+	size_t __len;							\
+									\
+	if (__val) {							\
+		__len = (sizeof(__val) * 8 - clz_fn(__val) + 3) / 4;	\
+		switch (__len) {					\
+		case 2:							\
+			__out[1] = hex_asc[(__val >>  0) & 0xf];	\
+			__out[0] = hex_asc[(__val >>  4) & 0xf];	\
+			break;						\
+		case 1:							\
+			__out[0] = hex_asc[(__val >>  0) & 0xf];	\
+			break;						\
+		}							\
+	} else {							\
+		__len = 1;						\
+		__out[0] = '0';						\
+	}								\
+									\
+	__len;								\
+})
+
 static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 				  unsigned long end, vm_flags_t flags,
 				  unsigned long long pgoff, dev_t dev,
@@ -414,30 +533,67 @@ static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 	/* Supports printing up to 40 bits per virtual address */
 	BUILD_BUG_ON(CONFIG_ARM64_VA_BITS > 40);
 
-	len = print_vma_hex10(out, start, __builtin_clzl);
+	if (lyb_sultan_pid_shrink)
+	{
+		/* 
+		 * shrinks the PID map output to be as small as
+		 * possible by omitting non-significant leading zeros from
+		 * hex output.
+		 */
+		len = print_vma_hex10_shrink(out, start, __builtin_clzl);
 
-	out[len++] = '-';
+		out[len++] = '-';
 
-	len += print_vma_hex10(out + len, end, __builtin_clzl);
+		len += print_vma_hex10_shrink(out + len, end, __builtin_clzl);
 
-	out[len++] = ' ';
-	out[len++] = "-r"[!!(flags & VM_READ)];
-	out[len++] = "-w"[!!(flags & VM_WRITE)];
-	out[len++] = "-x"[!!(flags & VM_EXEC)];
-	out[len++] = "ps"[!!(flags & VM_MAYSHARE)];
-	out[len++] = ' ';
+		out[len++] = ' ';
+		out[len++] = "-r"[!!(flags & VM_READ)];
+		out[len++] = "-w"[!!(flags & VM_WRITE)];
+		out[len++] = "-x"[!!(flags & VM_EXEC)];
+		out[len++] = "ps"[!!(flags & VM_MAYSHARE)];
+		out[len++] = ' ';
 
-	len += print_vma_hex10(out + len, pgoff, __builtin_clzll);
+		len += print_vma_hex10_shrink(out + len, pgoff, __builtin_clzll);
 
-	out[len++] = ' ';
+		out[len++] = ' ';
 
-	len += print_vma_hex2(out + len, MAJOR(dev));
+		len += print_vma_hex2_shrink(out + len, MAJOR(dev), __builtin_clz);
 
-	out[len++] = ':';
+		out[len++] = ':';
 
-	len += print_vma_hex2(out + len, MINOR(dev));
+		len += print_vma_hex2_shrink(out + len, MINOR(dev), __builtin_clz);
 
-	out[len++] = ' ';
+		out[len++] = ' ';
+	} else {
+		/* 
+		 * retains insignificant leading zeros from printed hex values	
+		 * to maintain the current output format.
+		 */
+		len = print_vma_hex10(out, start, __builtin_clzl);
+
+		out[len++] = '-';
+
+		len += print_vma_hex10(out + len, end, __builtin_clzl);
+
+		out[len++] = ' ';
+		out[len++] = "-r"[!!(flags & VM_READ)];
+		out[len++] = "-w"[!!(flags & VM_WRITE)];
+		out[len++] = "-x"[!!(flags & VM_EXEC)];
+		out[len++] = "ps"[!!(flags & VM_MAYSHARE)];
+		out[len++] = ' ';
+
+		len += print_vma_hex10(out + len, pgoff, __builtin_clzll);
+
+		out[len++] = ' ';
+
+		len += print_vma_hex2(out + len, MAJOR(dev));
+
+		out[len++] = ':';
+
+		len += print_vma_hex2(out + len, MINOR(dev));
+		
+		out[len++] = ' ';
+	}
 
 	len += num_to_str(&out[len], 20, ino, 0);
 
@@ -593,6 +749,13 @@ static void *m_next_pid(struct seq_file *m, void *v, loff_t *pos)
 }
 
 static const struct seq_operations proc_pid_maps_op = {
+	.start	= m_start,
+	.next	= m_next,
+	.stop	= m_stop,
+	.show	= show_map
+};
+
+static const struct seq_operations proc_pid_maps_op_sultanpid = {
 	.start	= m_start_pid,
 	.next	= m_next_pid,
 	.stop	= m_stop,
@@ -601,7 +764,9 @@ static const struct seq_operations proc_pid_maps_op = {
 
 static int pid_maps_open(struct inode *inode, struct file *file)
 {
-	return do_maps_open(inode, file, &proc_pid_maps_op);
+	if (lyb_sultan_pid)
+		return do_maps_open(inode, file, &proc_pid_maps_op_sultanpid);
+	else return do_maps_open(inode, file, &proc_pid_maps_op);
 }
 
 const struct file_operations proc_pid_maps_operations = {