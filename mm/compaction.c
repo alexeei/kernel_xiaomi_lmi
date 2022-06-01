4239252fa8af40503a3daad1cfe7581221a046a1
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-06-01 00:06:55 -0500

    Revert "mm: compaction: Run ZRAM compaction on automatic compaction"
    This reverts commit b746530f068a37d9aa58596e8d2ac293c8990b44.

--- mm/compaction.c
+++ mm/compaction.c
@@ -2506,8 +2506,6 @@ static void compact_nodes(void)
 }
 
 
-void zram_compact(void);
-
 static void do_compaction(struct work_struct *work)
 {
 	/* Return early if the screen is on */
@@ -2518,9 +2516,6 @@ static void do_compaction(struct work_struct *work)
 
 	/* Do full compaction */
 	compact_nodes();
-
-	/* Do ZRAM compaction */
-	zram_compact();
 
 	/* Force compaction timeout */
 	compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
