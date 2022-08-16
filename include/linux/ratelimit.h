d782c5926986bcd53b04e3036f655e434841383d
alexeei <megaxaser@gmail.com>
2022-08-16 12:04:13 +0300

    fix build 250

--- include/linux/ratelimit.h
+++ include/linux/ratelimit.h
@@ -7,41 +7,6 @@
 #include <linux/spinlock.h>
 
 
-#define DEFAULT_RATELIMIT_INTERVAL	(5 * HZ)
-#define DEFAULT_RATELIMIT_BURST		10
-
-/* issue num suppressed message on exit */
-#define RATELIMIT_MSG_ON_RELEASE	BIT(0)
-
-struct ratelimit_state {
-	raw_spinlock_t	lock;		/* protect the state */
-
-	int		interval;
-	int		burst;
-	int		printed;
-	int		missed;
-	unsigned long	begin;
-	unsigned long	flags;
-};
-
-#define RATELIMIT_STATE_INIT_FLAGS(name, interval_init, burst_init, flags_init) { \
-		.lock		= __RAW_SPIN_LOCK_UNLOCKED(name.lock),		  \
-		.interval	= interval_init,				  \
-		.burst		= burst_init,					  \
-		.flags		= flags_init,					  \
-	}
-
-#define RATELIMIT_STATE_INIT(name, interval_init, burst_init) \
-	RATELIMIT_STATE_INIT_FLAGS(name, interval_init, burst_init, 0)
-
-#define RATELIMIT_STATE_INIT_DISABLED					\
-	RATELIMIT_STATE_INIT(ratelimit_state, 0, DEFAULT_RATELIMIT_BURST)
-
-#define DEFINE_RATELIMIT_STATE(name, interval_init, burst_init)		\
-									\
-	struct ratelimit_state name =					\
-		RATELIMIT_STATE_INIT(name, interval_init, burst_init)	\
-
 
 static inline void ratelimit_state_init(struct ratelimit_state *rs,
 					int interval, int burst)
