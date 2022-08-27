8188f1a313ad8af13d816465aec6acda21f6770e
alexeei <megaxaser@gmail.com>
2022-08-27 13:20:17 +0300

    Revert "linux/random.h: Mark CONFIG_ARCH_RANDOM functions __must_check"
    This reverts commit 168338b9ecdc54449b9f802730bd5f51e61cb717.

--- include/linux/random.h
+++ include/linux/random.h
@@ -128,13 +128,18 @@ static inline bool __must_check arch_get_random_int(unsigned int *v)
 {
 	return false;
 }
-static inline bool __must_check arch_get_random_seed_long(unsigned long *v)
+static inline bool arch_get_random_seed_long(unsigned long *v)
 {
 	return false;
 }
+#endif
 
-static inline bool __must_check arch_get_random_seed_int(unsigned int *v)
- __must_check)
+/*
+ * Called from the boot CPU during startup; not valid to call once
+ * secondary CPUs are up and preemption is possible.
+ */
+#ifndef arch_get_random_seed_long_early
+static inline bool __init arch_get_random_seed_long_early(unsigned long *v)
 {
 	return false;
 }
