28f1b9611ecb3ff16a024affae5c104338356059
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-29 19:23:57 +0530

    sysctl: Allow maximum vm swappiness upto 200
    Change-Id: Ie0bb854db5834e2e2292682b927c5dcd8b5aaaac
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1774,7 +1774,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0444,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
-		.extra2		= &max_swappiness,
+		.extra2		= &two_hundred,
 	},
 	{
 		.procname       = "want_old_faultaround_pte",
