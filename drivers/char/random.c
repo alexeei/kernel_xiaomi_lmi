2bbb09687912611402ad0731ed20ad90e4e10001
Jason A. Donenfeld <Jason@zx2c4.com>
2022-11-17 17:47:12 +0100

    random: reseed in delayed work rather than on-demand
    Currently, we reseed when random bytes are requested, if the current
    seed is too old. Since random bytes can be requested from all contexts,
    including hard IRQ, this means sometimes we wind up adding a bit of
    latency to hard IRQ. This was so much of a problem on s390x that now
    s390x just doesn't provide its architectural RNG from hard IRQ context,
    so we miss out in that case.
    
    Instead, let's just schedule a persistent delayed work, so that the
    reseeding and potentially expensive operations will always happen from
    process context, reducing unexpected latencies from hard IRQ.
    
    This also has the nice effect of accumulating a transcript of random
    inputs over time, since it means that we amass more input values. And it
    should make future vDSO integration a bit easier.
    
    Cc: Harald Freudenberger <freude@linux.ibm.com>
    Cc: Juergen Christ <jchrist@linux.ibm.com>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Dominik Brodowski <linux@dominikbrodowski.net>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -276,27 +276,6 @@ static void crng_fast_key_erasure(u8 key[CHACHA20_KEY_SIZE],
 }
 
 /*
-
- * Return the interval until the next reseeding, which is normally
- * CRNG_RESEED_INTERVAL, but during early boot, it is at an interval
- * proportional to the uptime.
- */
-static unsigned int crng_reseed_interval(void)
-{
-	static bool early_boot = true;
-
-	if (unlikely(READ_ONCE(early_boot))) {
-		time64_t uptime = ktime_get_seconds();
-		if (uptime >= CRNG_RESEED_INTERVAL / HZ * 2)
-			WRITE_ONCE(early_boot, false);
-		else
-			return max_t(unsigned int, CRNG_RESEED_START_INTERVAL,
-				     (unsigned int)uptime / 2 * HZ);
-	}
-	return CRNG_RESEED_INTERVAL;
-}
-
-/*
  * This function returns a ChaCha state that you may use for generating
  * random data. It also returns up to 32 bytes on its own of random data
  * that may be used; random_data_len may not be greater than 32.
@@ -330,13 +309,6 @@ static void crng_make_state(u32 chacha_state[CHACHA20_BLOCK_SIZE / sizeof(u32)],
 		if (!ready)
 			return;
 	}
-
-	/*
-	 * If the base_crng is old enough, we reseed, which in turn bumps the
-	 * generation counter that we check below.
-	 */
-	if (unlikely(time_is_before_jiffies(READ_ONCE(base_crng.birth) + crng_reseed_interval())))
-		crng_reseed();
 
 	local_irq_save(flags);
 	crng = raw_cpu_ptr(&crngs);
@@ -664,7 +636,7 @@ static void __cold _credit_init_bits(size_t bits)
 	} while (cmpxchg(&input_pool.init_bits, orig, new) != orig);
 
 	if (orig < POOL_READY_BITS && new >= POOL_READY_BITS) {
-		crng_reseed(); /* Sets crng_init to CRNG_READY under base_crng.lock. */
+		crng_reseed(NULL); /* Sets crng_init to CRNG_READY under base_crng.lock. */
 		wake_up_interruptible(&crng_init_wait);
 		kill_fasync(&fasync, SIGIO, POLL_IN);
 		pr_notice("crng init done\n");
