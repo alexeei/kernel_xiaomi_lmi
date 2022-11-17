be9a81b316ed258f0416a17e3a890ff4174163c7
Jason A. Donenfeld <Jason@zx2c4.com>
2022-11-17 17:47:12 +0100

    random: reseed in delayed work rather than on-demand
    Currently, we reseed when random bytes are requested, if the current
    seed is too old. Since random bytes can be requested from all contexts,
    including hard IRQ, this means sometimes we wind up adding a bit of
    latency to hard IRQ. This was so much of a problem on s390x that now
    s390x just doesn't provide its architectural RNG from hard IRQ context,
    so we miss out in that case.
    
    Instead, let's just schedule a persistent delayed work, so that the
    reseeding and potentially expensive operations will always happen from
    process context, reducing unexpected latencies from hard IRQ.
    
    This also has the nice effect of accumulating a transcript of random
    inputs over time, since it means that we amass more input values. And it
    should make future vDSO integration a bit easier.
    
    Cc: Harald Freudenberger <freude@linux.ibm.com>
    Cc: Juergen Christ <jchrist@linux.ibm.com>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Dominik Brodowski <linux@dominikbrodowski.net>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -225,7 +225,6 @@ enum {
 
 static struct {
 	u8 key[CHACHA20_KEY_SIZE] __aligned(__alignof__(long));
-	unsigned long birth;
 	unsigned long generation;
 	spinlock_t lock;
 } base_crng = {
@@ -265,11 +264,16 @@ static unsigned int crng_reseed_interval(void)
 static void extract_entropy(void *buf, size_t len);
 
 /* This extracts a new crng key from the input pool. */
-static void crng_reseed(void)
+static void crng_reseed(struct work_struct *work)
 {
+	static DECLARE_DELAYED_WORK(next_reseed, crng_reseed);
 	unsigned long flags;
 	unsigned long next_gen;
 	u8 key[CHACHA20_KEY_SIZE];
+
+	/* Immediately schedule the next reseeding, so that it fires sooner rather than later. */
+	if (likely(system_unbound_wq))
+		queue_delayed_work(system_unbound_wq, &next_reseed, crng_reseed_interval());
 
 	extract_entropy(key, sizeof(key));
 
@@ -285,7 +289,6 @@ static void crng_reseed(void)
 	if (next_gen == ULONG_MAX)
 		++next_gen;
 	WRITE_ONCE(base_crng.generation, next_gen);
-	WRITE_ONCE(base_crng.birth, jiffies);
 	if (!crng_ready())
 		crng_init = CRNG_READY;
 	spin_unlock_irqrestore(&base_crng.lock, flags);
@@ -326,28 +329,7 @@ static void crng_fast_key_erasure(u8 key[CHACHA20_KEY_SIZE],
 }
 
 /*
- * Return whether the crng seed is considered to be sufficiently old
- * that a reseeding is needed. This happens if the last reseeding
- * was CRNG_RESEED_INTERVAL ago, or during early boot, at an interval
- * proportional to the uptime.
- */
-static bool crng_has_old_seed(void)
-{
-	static bool early_boot = true;
-	unsigned long interval = CRNG_RESEED_INTERVAL;
 
-	if (unlikely(READ_ONCE(early_boot))) {
-		time64_t uptime = ktime_get_seconds();
-		if (uptime >= CRNG_RESEED_INTERVAL / HZ * 2)
-			WRITE_ONCE(early_boot, false);
-		else
-			interval = max_t(unsigned int, CRNG_RESEED_START_INTERVAL,
-					 (unsigned int)uptime / 2 * HZ);
-	}
-	return time_is_before_jiffies(READ_ONCE(base_crng.birth) + interval);
-}
-
-/*
  * This function returns a ChaCha state that you may use for generating
  * random data. It also returns up to 32 bytes on its own of random data
  * that may be used; random_data_len may not be greater than 32.
@@ -382,12 +364,6 @@ static void crng_make_state(u32 chacha_state[CHACHA20_BLOCK_SIZE / sizeof(u32)],
 			return;
 	}
 
-	/*
-	 * If the base_crng is old enough, we reseed, which in turn bumps the
-	 * generation counter that we check below.
-	 */
-	if (unlikely(crng_has_old_seed()))
-		crng_reseed();
 
 	local_irq_save(flags);
 	crng = raw_cpu_ptr(&crngs);
@@ -742,8 +718,7 @@ static void __cold _credit_init_bits(size_t bits)
 	} while (cmpxchg(&input_pool.init_bits, orig, new) != orig);
 
 	if (orig < POOL_READY_BITS && new >= POOL_READY_BITS) {
-		crng_reseed(); /* Sets crng_init to CRNG_READY under base_crng.lock. */
-		process_random_ready_list();
+		crng_reseed(NULL); /* Sets crng_init to CRNG_READY under base_crng.lock. */
 		wake_up_interruptible(&crng_init_wait);
 		kill_fasync(&fasync, SIGIO, POLL_IN);
 		pr_notice("crng init done\n");
@@ -859,7 +834,7 @@ int __init random_init(const char *command_line)
 	add_latent_entropy();
 
 	if (crng_ready())
-		crng_reseed();
+		crng_reseed(NULL);
 	else if (trust_cpu)
 		_credit_init_bits(arch_bits);
 
@@ -1396,7 +1371,7 @@ static long random_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
 			return -EPERM;
 		if (!crng_ready())
 			return -ENODATA;
-		crng_reseed();
+		crng_reseed(NULL);
 		return 0;
 	default:
 		return -EINVAL;
