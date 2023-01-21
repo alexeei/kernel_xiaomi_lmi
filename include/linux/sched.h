95335837d368b7a5cede0ba4cf0afd7e2222e1d6
alexeei <megaxaser@gmail.com>
2023-01-21 15:40:49 +0300

    sched: Add sched_cpu_util part2

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2102,6 +2102,15 @@ static inline unsigned long sched_cpu_util(int cpu)
 }
 #endif
 
+#ifdef CONFIG_CPU_FREQ_GOV_SCHEDHORIZON
+unsigned long sched_cpu_util(int cpu);
+#else
+static inline unsigned long sched_cpu_util(int cpu)
+{
+	return 0;
+}
+#endif
+
 #ifdef CONFIG_RSEQ
 
 /*
