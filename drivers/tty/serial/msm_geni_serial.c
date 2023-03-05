fe4884e72a15db4e60a8cc03a8bb752a481402b5
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-05 23:41:44 +0600

    drivers: tty: Release wakelock when serial unregisters or fails
    The wakelock is held and is never destroyed.
    
    Change-Id: Id55c96c922a5cc5b311d822a48f889a12bdf654e
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -3632,6 +3632,7 @@ exit_wakeup_unregister:
 		wakeup_source_unregister(dev_port->geni_wake);
 exit_geni_serial_probe:
 	IPC_LOG_MSG(dev_port->ipc_log_misc, "%s: ret:%d\n", __func__, ret);
+	wakeup_source_unregister(dev_port->geni_wake);
 	return ret;
 }
 
