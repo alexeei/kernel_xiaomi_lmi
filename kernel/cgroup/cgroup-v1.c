16bb2d308513f66e232ef2f240a93e3ab80ca406
alexeei <megaxaser@gmail.com>
2023-05-11 18:34:56 +0300

    fix build

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -13,10 +13,10 @@
 #include <linux/delayacct.h>
 #include <linux/pid_namespace.h>
 #include <linux/cgroupstats.h>
+#include <linux/cpu.h>
 #include <linux/binfmts.h>
 #include <linux/devfreq_boost.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/cpu.h>
 
 
 #include <trace/events/cgroup.h>
