8b82d9c0fc6ab69019316a6a7a2901558dce5d28
alexeei <megaxaser@gmail.com>
2023-06-03 11:36:44 +0300

    improve kernelsu integration

--- fs/open.c
+++ fs/open.c
@@ -358,6 +358,7 @@ extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int
  */
 long do_faccessat(int dfd, const char __user *filename, int mode)
 {
+    ksu_handle_faccessat(&dfd, &filename, &mode, NULL);  // call KSU hook first
 	const struct cred *old_cred;
 	struct cred *override_cred;
 	struct path path;
