98deca4d4ed72acc8ef7883ef6f9fe7d4160434e
alexeei <megaxaser@gmail.com>
2023-10-01 22:18:43 +0300

    squash of changes in defconfig

--- include/linux/mm_event.h
+++ include/linux/mm_event.h
@@ -24,9 +24,17 @@ struct mm_event_task {
 	u64 accm_lat;
 } __attribute__ ((packed));
 
+#ifdef CONFIG_MM_EVENT_STAT
+void mm_event_task_init(struct task_struct *tsk);
+void mm_event_start(ktime_t *time);
+void mm_event_end(enum mm_event_type event, ktime_t start);
+void mm_event_count(enum mm_event_type event, int count);
+#else
 static inline void mm_event_task_init(struct task_struct *tsk) {}
 static inline void mm_event_start(ktime_t *time) {}
 static inline void mm_event_end(enum mm_event_type event, ktime_t start) {}
 static inline void mm_event_count(enum mm_event_type event, int count) {}
+#endif /* _LINUX_MM_EVENT_H */
+
 
 #endif
