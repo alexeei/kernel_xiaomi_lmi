fb56ea5f309cbe59b75f2f982c872bcb79d86d0b
alexeei <megaxaser@gmail.com>
2023-11-04 21:14:21 +0300

    fix missing endif

--- include/linux/bits.h
+++ include/linux/bits.h
@@ -17,7 +17,7 @@
 
 #define BIT_ULL(nr)		(ULL(1) << (nr))
 #define BIT_MASK(nr)		(UL(1) << ((nr) % BITS_PER_LONG))
-
+#endif
 #define BIT_WORD(nr)		((nr) / BITS_PER_LONG)
 #define BIT_ULL_MASK(nr)	(ULL(1) << ((nr) % BITS_PER_LONG_LONG))
 #define BIT_ULL_WORD(nr)	((nr) / BITS_PER_LONG_LONG)
