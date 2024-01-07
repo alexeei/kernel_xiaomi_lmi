adb5629084d76940869da5d28d5367e2e565973f
alexeei <megaxaser@gmail.com>
2024-01-07 16:38:17 +0300

    sched/debug: Remove the /proc/sys/kernel/sched_child_runs_first sysctl

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -95,11 +95,6 @@ unsigned int sysctl_sched_base_slice			= 750000ULL;
 unsigned int normalized_sysctl_sched_base_slice		= 750000ULL;
 
 
-/*
- * After fork, child runs first. If set to 0 (default) then
- * parent will (try to) run first.
- */
-unsigned int sysctl_sched_child_runs_first __read_mostly;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
 
